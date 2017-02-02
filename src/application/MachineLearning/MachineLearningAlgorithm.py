483e34c3db901e8e120ccda37488262eec222a96
Simone90SSL <simone_ro@hotmail.it>
2017-02-02 09:59:20 +0100

    Defined a modul to get different inputs for our machine learning algorithms

--- src/application/MachineLearning/MachineLearningAlgorithm.py
+++ src/application/MachineLearning/MachineLearningAlgorithm.py
@@ -1,9 +1,7 @@
 import random
-import numpy as np
-import src.application.Domain.League as League
-import src.application.Domain.Team as Team
 
 from src.application.MachineLearning.my_sklearn.Sklearn import SklearnAlgorithm
+import src.application.MachineLearning.MachineLearningInput as MLInput
 
 class MachineLearningAlgorithm(object):
     def __init__(self, framework, algorithm, data, data_label, train_percentage=0.75, data_description=None):
@@ -49,7 +47,7 @@ class MachineLearningAlgorithm(object):
         print("AAA accuracy", accuracy/len(predicted_labels))
 
         for k, v in enumerate(predicted_labels):
-            print(self.train_description[k],"\t",self.test_label[k], "\t", v, "\t", probability_events[k])
+            print(self.test_description[k],"\t",self.test_label[k], "\t", v, "\t", probability_events[k])
 
 
     def predict(self, data):
@@ -84,49 +82,7 @@ def split_data(split_percentage=0.75, shuffle=True, *datas):
     return train_datas, test_datas
 
 def test():
-    italy_league = League.read_all()[4]
-    teams = italy_league.get_teams("2015/2016")
-
-    goal_done = {team.team_long_name: 0 for team in teams}
-    goal_received = {team.team_long_name: 0 for team in teams}
-
-    for team in teams:
-        t = Team.read_by_name(team.team_long_name)
-        matches = t.get_matches(ordered=True, season="2015/2016")
-        for m in matches:
-            if team.team_long_name == m.get_home_team().team_long_name:
-                goal_done[team.team_long_name] = goal_done[team.team_long_name] + m.home_team_goal
-                goal_received[team.team_long_name] = goal_received[team.team_long_name] + m.away_team_goal
-            else:
-                goal_done[team.team_long_name] = goal_done[team.team_long_name] + m.away_team_goal
-                goal_received[team.team_long_name] = goal_received[team.team_long_name] + m.home_team_goal
-
-    # represent a match as [HTGD, HTGR, ATGD, ATGR], label them with 1 (home team win), -1 oterwise
-
-    matches = []
-    labels = []
-    matches_names = []
-    for match in italy_league.get_matches(season="2015/2016"):
-        home_team = match.get_home_team()
-        away_team = match.get_away_team()
-        label = 0
-        if match.home_team_goal > match.away_team_goal:
-            labels.append(1)
-        else:
-            labels.append(0)
-        # matches.append([goal_done[home_team.team_long_name],goal_received[home_team.team_long_name],goal_done[away_team.team_long_name],goal_received[away_team.team_long_name]])
-        home_goal_done, home_goal_received = home_team.get_goals_by_season_and_stage("2015/2016", match.stage)
-        # print(match.stage, home_team.team_long_name, home_goal_done, home_goal_received)
-        away_goal_done, away_goal_received = away_team.get_goals_by_season_and_stage("2015/2016", match.stage)
-        matches.append(np.asarray([home_goal_done / match.stage, home_goal_received / match.stage,
-                                   home_team.get_points_by_season_and_stage("2015/2016", match.stage) / match.stage,
-                                   away_goal_done / match.stage, away_goal_received / match.stage,
-                                   away_team.get_points_by_season_and_stage("2015/2016", match.stage) / match.stage]))
-
-        matches_names.append(home_team.team_long_name + " vs " + away_team.team_long_name)
-
-    matches = np.asarray(matches)
-    labels = np.asarray(labels)
+    matches, labels, matches_names = MLInput.get_datas()
     mag = MachineLearningAlgorithm("Sklearn","SVC", matches, labels, train_percentage=0.75, data_description=matches_names)
     mag.train()
     mag.score()
---
+++ src/application/MachineLearning/MachineLearningInput.py
@@ -0,0 +1,47 @@
+import numpy as np
+import src.application.Domain.League as League
+import src.application.Domain.Team as Team
+
+def get_datas():
+    italy_league = League.read_all()[4]
+    teams = italy_league.get_teams("2015/2016")
+
+    goal_done = {team.team_long_name: 0 for team in teams}
+    goal_received = {team.team_long_name: 0 for team in teams}
+
+    for team in teams:
+        t = Team.read_by_name(team.team_long_name)
+        matches = t.get_matches(ordered=True, season="2015/2016")
+        for m in matches:
+            if team.team_long_name == m.get_home_team().team_long_name:
+                goal_done[team.team_long_name] = goal_done[team.team_long_name] + m.home_team_goal
+                goal_received[team.team_long_name] = goal_received[team.team_long_name] + m.away_team_goal
+            else:
+                goal_done[team.team_long_name] = goal_done[team.team_long_name] + m.away_team_goal
+                goal_received[team.team_long_name] = goal_received[team.team_long_name] + m.home_team_goal
+
+    matches = []
+    labels = []
+    matches_names = []
+    for match in italy_league.get_matches(season="2015/2016"):
+        home_team = match.get_home_team()
+        away_team = match.get_away_team()
+        label = 0
+        if match.home_team_goal > match.away_team_goal:
+            labels.append(1)
+        else:
+            labels.append(0)
+        # matches.append([goal_done[home_team.team_long_name],goal_received[home_team.team_long_name],goal_done[away_team.team_long_name],goal_received[away_team.team_long_name]])
+        home_goal_done, home_goal_received = home_team.get_goals_by_season_and_stage("2015/2016", match.stage)
+        # print(match.stage, home_team.team_long_name, home_goal_done, home_goal_received)
+        away_goal_done, away_goal_received = away_team.get_goals_by_season_and_stage("2015/2016", match.stage)
+        matches.append(np.asarray([home_goal_done / match.stage, home_goal_received / match.stage,
+                                   home_team.get_points_by_season_and_stage("2015/2016", match.stage) / match.stage,
+                                   away_goal_done / match.stage, away_goal_received / match.stage,
+                                   away_team.get_points_by_season_and_stage("2015/2016", match.stage) / match.stage]))
+
+        matches_names.append(home_team.team_long_name + " vs " + away_team.team_long_name)
+
+    matches = np.asarray(matches)
+    labels = np.asarray(labels)
+    return matches, labels, matches_names
