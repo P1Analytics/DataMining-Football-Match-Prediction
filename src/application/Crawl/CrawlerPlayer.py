deefe48895579fa46074b7574e2ac6e035ad7e9f
Simone90SSL <simone_ro@hotmail.it>
2017-02-08 12:28:23 +0100

    Crawling

--- src/application/Crawl/CrawlerPlayer.py
+++ src/application/Crawl/CrawlerPlayer.py
@@ -1,10 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-import src.util.util as util
-import src.application.Domain.Team as Team
-
-from src.application.Domain.Player import Player
+import calendar
 
 
 class CrawlerPlayer(object):
@@ -19,13 +16,40 @@ class CrawlerPlayer(object):
     def start_crawling(self):
         if not self.player:
             # looking for player name and fifa_api_id
-            player_name, player_fifa_api_id = self.look_for_base_data()
-            print(player_name, player_fifa_api_id)
+            player_name, player_fifa_api_id, birthday, height, weight = self.look_for_base_data()
+
+
+
 
 
     def look_for_base_data(self):
-        h1 = self.soup.find('div', {"class": "info"}).h1
+        div = self.soup.find('div', {"class": "info"})
+
+        # name and fifa api id
+        h1 = div.h1
         data_string = str(h1.string)
         player_long_name = data_string[0:data_string.index("(")].strip()
         player_fifa_api_id = data_string[data_string.index("(") + 1:-1].split(" ")[1]
-        return player_long_name, int(player_fifa_api_id)
+
+        # birthday height and weight
+        meta_text = str(div.find('div', {"class":"meta"}).get_text())
+        row_birthday = meta_text[meta_text.index("(")+1:meta_text.index(")")]
+        birthday = get_db_birthday_date(row_birthday)
+
+
+
+
+        return player_long_name, int(player_fifa_api_id), birthday+" 00:00:00", 0, 0
+
+
+def get_db_birthday_date(birthday_text):
+    month_str = birthday_text.split(" ")[0]
+
+    day = int(birthday_text.split(" ")[1][:-1])
+    month = {v: k for k,v in enumerate(calendar.month_abbr)}[month_str]
+
+    year = birthday_text.split(" ")[2]
+    return year+"-"+'{0:02d}'.format(month)+"-"+'{0:02d}'.format(day)
+
+
+
--- src/test/Test_crawling.py
+++ src/test/Test_crawling.py
@@ -1,3 +1,8 @@
 import src.application.Crawl.Crawler as Crawler
 
+import src.application.Domain.Player as Player
+
 Crawler.start_crawling()
+
+#for p in Player.read_all():
+#    print(p.birthday)
