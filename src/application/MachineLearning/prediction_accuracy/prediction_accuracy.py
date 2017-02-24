90307ae0cf1c75acd5e3c9525ae3910af7e0f2b7
Simone90SSL <simone_ro@hotmail.it>
2017-02-24 12:36:55 +0100

    test accuracy, almost ready for experiment
    Signed-off-by: Simone90SSL <simone_ro@hotmail.it>

--- src/application/MachineLearning/prediction_accuracy/prediction_accuracy.py
+++ src/application/MachineLearning/prediction_accuracy/prediction_accuracy.py
@@ -6,119 +6,129 @@ import numpy as np
 from src.application.Exception.MLException import MLException
 
 
-def get_prediction_accuracy(league, only_team_history=False, **params):
+class PredictionAccuracy(object):
+    def __init__(self,league, only_team_history=False, **params):
+        self.league = league
+        self.only_team_history = only_team_history
 
-    season = util.get_default(params, "season", util.get_current_season())
-    stages = util.get_default(params, "stages", [s for s in range(1, 39)])
-    representation = util.get_default(params, "representation", 1)
+        self.season = util.get_default(params, "season", util.get_current_season())
+        self.stages = util.get_default(params, "stages", [s for s in range(1, 39)])
+        self.representation = util.get_default(params, "representation", 1)
 
-    ml_train_stages_to_train = util.get_default(params, "ml_train_stages_to_train", 10)
-    ml_train_input_id = util.get_default(params, "ml_train_input_id", 1)
+        self.ml_train_stages_to_train = util.get_default(params, "ml_train_stages_to_train", 10)
+        self.ml_train_input_id = util.get_default(params, "ml_train_input_id", 1)
 
-    ml_alg_framework = util.get_default(params, "ml_alg_framework", "Sklearn")
-    ml_alg_method = util.get_default(params, "ml_alg_method", "SVM")
+        self.ml_alg_framework = util.get_default(params, "ml_alg_framework", "Sklearn")
+        self.ml_alg_method = util.get_default(params, "ml_alg_method", "SVM")
 
-    ml_alg_params = dict()
-    ml_alg_params["number_step"] = util.get_default(params, "ml_alg_number_step", 1000)
-    ml_alg_params["kernel"] = util.get_default(params, "ml_alg_kernel", "rbf")
-    ml_alg_params["k"] = util.get_default(params, "ml_alg_k", 9)
+        self.ml_alg_params = dict()
+        self.ml_alg_params["number_step"] = util.get_default(params, "ml_alg_number_step", 1000)
+        self.ml_alg_params["kernel"] = util.get_default(params, "ml_alg_kernel", "rbf")
+        self.ml_alg_params["k"] = util.get_default(params, "ml_alg_k", 9)
 
-    print("Season:", season)
-    print("Stages:", stages)
-    print("ml_train_stages_to_train", ml_train_stages_to_train)
-    print("ml_train_input_id", ml_train_input_id)
-    print("only_team_history", only_team_history)
+        self.accuracy_stage_dic = dict()
 
-    accuracy_stage = dict()
-    for stage in stages:
-        print(accuracy_stage)
-        print()
-        print(season, stage)
-        print()
+        print("Season:", self.season)
+        print("Stages:", self.stages)
+        print("ml_train_stages_to_train", self.ml_train_stages_to_train)
+        print("ml_train_input_id", self.ml_train_input_id)
+        print("only_team_history", self.only_team_history)
 
-        if only_team_history:
-            # train ml algorithm only with past mathces of teams
-            for match in league.get_matches(season=season, stage=stage):
-                home_team = match.get_home_team()
-                away_team = match.get_away_team()
+    def get_prediction_accuracy(self):
+        for stage in self.stages:
+            print(self.accuracy_stage_dic)
+            print()
+            print(self.season, stage)
+            print()
+
+            if self.only_team_history:
+                # train ml algorithm only with past mathces of teams
+                for match in self.league.get_matches(season=self.season, stage=stage):
+                    home_team = match.get_home_team()
+                    away_team = match.get_away_team()
+                    try:
+                        home_matches, \
+                        home_labels, \
+                        home_matches_names, \
+                        home_matches_to_predict, \
+                        home_matches_to_predict_names, \
+                        home_labels_to_predict = \
+                            MLInput.get_input_to_train(self.ml_train_input_id,
+                                                       home_team,
+                                                       self.representation,
+                                                       stage,
+                                                       stages_to_train=self.ml_train_stages_to_train,
+                                                       season=self.season)
+
+                        away_matches, away_labels, away_matches_names, away_matches_to_predict, \
+                        away_matches_to_predict_names, away_labels_to_preidct = \
+                            MLInput.get_input_to_train(self.ml_train_input_id,
+                                                       away_team,
+                                                       self.representation,
+                                                       stage,
+                                                       stages_to_train=self.ml_train_stages_to_train,
+                                                       season=self.season)
+
+                        self.matches = np.concatenate((home_matches, away_matches), axis=0)
+                        self.labels = np.concatenate((home_labels, away_labels), axis=0)
+                        self.matches_names = home_matches_names.extend(away_matches_names)
+                        self.matches_to_predict = home_matches_to_predict               # the same works with away
+                        self.matches_to_predict_names = home_matches_to_predict_names   # the same works with away
+                        self.labels_to_predict = home_labels_to_predict                 # the same works with away
+                    except MLException as mle:
+                        print()
+                        print(mle, home_team.team_long_name, away_team.team_long_name)
+                        print()
+                        continue
+
+                    self.train_predict()
+            else:
+                # train ml algorithm with all league matches
                 try:
-                    home_matches, home_labels, home_matches_names, home_matches_to_predict, \
-                    home_matches_to_predict_names, home_labels_to_preidct = \
-                        MLInput.get_input_to_train(ml_train_input_id,
-                                                   home_team,
-                                                   representation,
-                                                   stage,
-                                                   stages_to_train=ml_train_stages_to_train,
-                                                   season=season)
-
-                    away_matches, away_labels, away_matches_names, away_matches_to_predict, \
-                    away_matches_to_predict_names, away_labels_to_preidct = \
-                        MLInput.get_input_to_train(ml_train_input_id,
-                                                   away_team,
-                                                   representation,
-                                                   stage,
-                                                   stages_to_train=ml_train_stages_to_train,
-                                                   season=season)
-
-                    matches = np.concatenate((home_matches, away_matches), axis=0)
-                    labels = np.concatenate((home_labels, away_labels), axis=0)
-                    matches_names = home_matches_names.extend(away_matches_names)
+                    self.matches, \
+                    self.labels, \
+                    self.matches_names, \
+                    self.matches_to_predict, \
+                    self.matches_to_predict_names, \
+                    self.labels_to_preidct = MLInput.get_input_to_train(self.ml_train_input_id,
+                                                                   self.league,
+                                                                   self.representation,
+                                                                   stage,
+                                                                   stages_to_train=self.ml_train_stages_to_train,
+                                                                   season=self.season)
                 except MLException as mle:
-                    print()
-                    print(mle, home_team.team_long_name, away_team.team_long_name)
-                    print()
+                    print(mle)
                     continue
 
-                train_predict(labels, home_labels_to_preidct, matches, matches_names, home_matches_to_predict,
-                              home_matches_to_predict_names, ml_alg_framework, ml_alg_method, ml_alg_params,
-                              accuracy_stage)
-        else:
-            # train ml algorithm with all league matches
-            try:
-                matches, labels, matches_names, matches_to_predict, matches_to_predict_names, \
-                labels_to_preidct = MLInput.get_input_to_train(ml_train_input_id,
-                                                               league,
-                                                               representation,
-                                                               stage,
-                                                               stages_to_train=ml_train_stages_to_train,
-                                                               season=season)
-            except MLException as mle:
-                print(mle)
-                continue
+                self.train_predict()
 
-            train_predict(labels, labels_to_preidct, matches, matches_names, matches_to_predict,
-                          matches_to_predict_names, ml_alg_framework, ml_alg_method, ml_alg_params,
-                          accuracy_stage)
+    def train_predict(self):
+        ml_alg = MachineLearningAlgorithm.get_machine_learning_algorithm(self.ml_alg_framework,
+                                                                         self.ml_alg_method,
+                                                                         self.matches,
+                                                                         self.labels,
+                                                                         data_description=self.matches_names,
+                                                                         train_percentage=1,
+                                                                         **self.ml_alg_params)
+        ml_alg.train()
+        predicted_labels, probability_events = ml_alg.predict(self.matches_to_predict)
+        accuracy = 0
+        team_accuracy_dic = {}
+        for p, l, s in zip(predicted_labels, self.labels_to_predict, self.matches_to_predict_names):
+            print("*****")
+            print(s)
+            s = s.split("vs")
+            print('\tResult:', l, "\tPredicted", p)
+            print("*****")
+            if p == l:
+                accuracy += 1
+                team_accuracy_dic[s[0].strip()] = [accuracy, 1]
+                team_accuracy_dic[s[1].strip()] = [accuracy, 1]
+            else:
+                team_accuracy_dic[s[0].strip()] = [accuracy, 1]
+                team_accuracy_dic[s[1].strip()] = [accuracy, 1]
 
-
-def train_predict(labels, labels_to_predict, matches, matches_names, matches_to_predict, matches_to_predict_names,
-                  ml_alg_framework, ml_alg_method, ml_alg_params, accuracy_stage_dic):
-    ml_alg = MachineLearningAlgorithm.get_machine_learning_algorithm(ml_alg_framework,
-                                                                     ml_alg_method,
-                                                                     matches,
-                                                                     labels,
-                                                                     data_description=matches_names,
-                                                                     train_percentage=1,
-                                                                     **ml_alg_params)
-    ml_alg.train()
-    predicted_labels, probability_events = ml_alg.predict(matches_to_predict)
-    accuracy = 0
-    team_accuracy_dic = {}
-    for p, l, s in zip(predicted_labels, labels_to_predict, matches_to_predict_names):
-        print("*****")
-        print(s)
-        s = s.split("vs")
-        print('\tResult:', l, "\tPredicted", p)
-        print("*****")
-        if p == l:
-            accuracy += 1
-            team_accuracy_dic[s[0].strip()] = [accuracy, 1]
-            team_accuracy_dic[s[1].strip()] = [accuracy, 1]
-        else:
-            team_accuracy_dic[s[0].strip()] = [accuracy, 1]
-            team_accuracy_dic[s[1].strip()] = [accuracy, 1]
-
-    update_team_accuracy(accuracy_stage_dic, team_accuracy_dic)
+        update_team_accuracy(self.accuracy_stage_dic, team_accuracy_dic)
 
 
 def update_team_accuracy(team_accuracy_dic, stage_accuracy_dic):
--- src/test/Test.py
+++ src/test/Test.py
@@ -1,4 +1,4 @@
-import src.application.MachineLearning.prediction_accuracy.prediction_accuracy as pa
+from src.application.MachineLearning.prediction_accuracy.prediction_accuracy import PredictionAccuracy
 import src.application.Domain.League as League
 
 def doTest():
@@ -7,7 +7,8 @@ def doTest():
     params["season"] = '2015/2016'
     params["ml_train_input_id"] = 2
     italy_league = League.read_by_name("Italy", like=True)[0]
-    pa.get_prediction_accuracy(italy_league, only_team_history =True, **params)
+    pa = PredictionAccuracy(italy_league, only_team_history =True, **params)
+    pa.get_prediction_accuracy()
 
 doTest()
 
