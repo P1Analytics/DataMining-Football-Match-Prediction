8c8893a62a7ff411a5425de562f704cece1faf25
nanazhu <nanazhu@users.noreply.github.com>
2017-03-02 23:57:08 +0100

    Update Test.py

--- src/test/Test.py
+++ src/test/Test.py
@@ -5,22 +5,22 @@ import src.util.util as util
 util.init_logger()
 
 def do_test_1():
-    italy_league = League.read_by_name("Italy", like=True)[0]
+    league = League.read_by_name("Italy", like=True)[0]
     for mtii in [1, 2, 3]:
         for mtir in [1,2,3,4]:
             if mtii == 1 and (mtir == 1 or mtir == 2):
                 continue
             params = {"ml_train_input_id": mtii, "ml_train_input_representation": mtir}
             exp = Experiment(1)
-            exp.run(italy_league, complete=False, **params)
+            exp.run(league, complete=False, **params)
 
 
 def doTest():
-    italy_league = League.read_by_name("Italy", like=True)[0]
+    league = League.read_by_name("Italy", like=True)[0]
     exp = Experiment(4)
     params = {"ml_alg_method":"AdaBoostClassifier", "ml_alg_framework":"my_poisson", "ml_train_stages_to_train":20,
               "ml_train_input_id":5}
-    exp.run(italy_league, complete=False, **params)
+    exp.run(league, complete=False, **params)
 
 
 doTest()
